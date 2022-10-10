6fda44e49354ff8b4311d400534d8a7bc441688a
gcosentino1 <85801475+joeycose@users.noreply.github.com>
2022-10-09 22:48:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/************************************************************************* * BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part * of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students. *
+* Name: Giusepp Cosentino Student ID: 184801215 Date: Sun Oct 9, 2022 *
+* Your app’s URL (from Cyclic) : ______________________________________________ * *************************************************************************/
+
 var dataService = require('./data-service'); // require data-service file 
 
 var express = require('express'); // Express web server framework
